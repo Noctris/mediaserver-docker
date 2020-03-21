92b25f2a64200bc5242762f38c0bf779ad355a9b
michel.van.hoof <mcvh@xylos.com>
2020-03-21 10:22:01 +0100

    Added phpipam

--- lab/up.sh
+++ lab/up.sh
@@ -4,5 +4,6 @@ cp traefik-servers/* /mnt/dockervol/traefik/servers/
 sudo docker-compose -f docker-compose.yml \
 -f management/portainer.yml \
 -f management/phpmyadmin.yml \
+-f management/phpipam.yml \
 -f services/mariadb_test.yml \
 up -d
