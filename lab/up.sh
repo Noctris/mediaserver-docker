d9e0024abd43b773648871df34a6580157e99631
michel.van.hoof <mcvh@xylos.com>
2020-03-21 09:43:15 +0100

    Added PhpMyAdmin

--- lab/up.sh
+++ lab/up.sh
@@ -3,4 +3,5 @@ rm /mnt/dockervol/traefik/servers/*
 cp traefik-servers/* /mnt/dockervol/traefik/servers/
 sudo docker-compose -f docker-compose.yml \
 -f management/portainer.yml \
+-f management/phpmyadmin.yml \
 up -d
