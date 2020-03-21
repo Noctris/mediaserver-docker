a2acf67302c291d1d3520391cb8dcc43cd45a8a9
michel.van.hoof <mcvh@xylos.com>
2020-03-21 10:13:59 +0100

    Forgot to include it in the up script

--- lab/up.sh
+++ lab/up.sh
@@ -4,4 +4,5 @@ cp traefik-servers/* /mnt/dockervol/traefik/servers/
 sudo docker-compose -f docker-compose.yml \
 -f management/portainer.yml \
 -f management/phpmyadmin.yml \
+-f services/mariadb_test.yml \
 up -d
