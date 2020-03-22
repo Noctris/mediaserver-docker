276629d6924ba700bb90eec1d458dfb037584d4c
michel.van.hoof <mcvh@xylos.com>
2020-03-22 21:32:41 +0100

    Adding Jackett

--- home/up.sh
+++ home/up.sh
@@ -6,6 +6,7 @@ sudo docker-compose -f docker-compose.yml \
 -f management/phpmyadmin.yml \
 -f services/mariadb_test.yml \
 -f services/nntp2nntp.yml \
+-f services/jackett.yml \
 -f infra/influxdb.yml \
 -f infra/mariadb.yml \
 -f PVR/radarr_parents.yml \
