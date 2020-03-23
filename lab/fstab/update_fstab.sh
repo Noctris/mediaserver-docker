3e7c5f214200f1a01acd5de9691833b43e665c89
michel.van.hoof <mcvh@xylos.com>
2020-03-23 22:35:32 +0100

    Upgrade to Azure dns challenge

---
+++ lab/fstab/update_fstab.sh
@@ -0,0 +1,8 @@
+sudo mkdir /mnt/media_parents
+sudo mkdir /mnt/downloads_parents
+sudo mkdir /mnt/media_family
+sudo mkdir /mnt/downloads_family
+sudo mkdir /mnt/media_kids
+sudo mkdir /mnt/downloads_kids
+sudo mkdir /mnt/docker_prod
+sudo mount -a
