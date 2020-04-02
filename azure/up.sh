5db5d781d089bf8e1884fe90b6612a9e75896c01
Michel Van Hoof <Michel.Van.Hoof@xylos.com>
2020-04-02 16:22:02 +0200

    UPdated scrip

--- azure/up.sh
+++ azure/up.sh
@@ -5,9 +5,5 @@ sudo docker-compose -f docker-compose.yml \
 -f management/portainer.yml \
 -f management/phpmyadmin.yml \
 -f management/phpipam.yml \
--f services/mariadb_test.yml \
--f infra/influxdb.yml \
--f infra/mariadb.yml \
--f PVR/radarr_parents.yml \
--f PVR/sonarr_parents.yml \
+-f services/heimdall.yml \
 up -d
