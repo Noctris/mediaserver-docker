27d5dd5ce9b2a617960a7d5488669c1cff5c7f9e
Michel Van Hoof <Michel.Van.Hoof@xylos.com>
2020-04-03 16:30:34 +0200

    Phpipam and phpmyadmin

--- azure/up.sh
+++ azure/up.sh
@@ -3,6 +3,8 @@ git pull
 # cp traefik-config/* /mnt/dockervol/traefik/servers/
 sudo docker-compose -f docker-compose.yml \
 -f management/portainer.yml \
+-f management/phpipam.yml \
+-f management/phpmyadmin.yml \
 -f monitoring/zabbix.yml \
 -f services/heimdall.yml \
 -f infrastructure/mariadb.yml \
