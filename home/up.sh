1c92933a5ed9746c282f3441e663fbf595989756
michel.van.hoof <mcvh@xylos.com>
2020-04-19 18:51:35 +0200

    Fixed nfs stuff

--- home/up.sh
+++ home/up.sh
@@ -1,6 +1,6 @@
 git pull 
-#rm /mnt/dockervol/traefik/servers/*
-#cp traefik-config/* /mnt/dockervol/traefik/servers/
+rm /mnt/dockervol/traefik/servers/*
+cp traefik-config/* /mnt/dockervol/traefik/servers/
 sudo docker-compose -f docker-compose.yml \
 -f management/portainer.yml \
 -f management/phpmyadmin.yml \
