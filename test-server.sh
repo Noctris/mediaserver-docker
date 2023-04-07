afe813b75578461d55d3eac2914ce2e44e8745b1
Michel Van Hoof <m-vanhoof@lbctt.com>
2023-04-07 17:19:51 +0200

    Setup dozzle so I can monitor

--- test-server.sh
+++ test-server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #docker-compose -f traefik/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+docker-compose -f monitoring/dozzle/docker-compose.yaml --env-file ../.env up -d --remove-orphans
 docker-compose -f portainer-ee/docker-compose.yaml --env-file ../.env up -d --remove-orphans
