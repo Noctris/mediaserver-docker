fcb0531a64e257834669c1c5805b79fef61f9fb4
Michel Van Hoof <m-vanhoof@lbctt.com>
2023-04-07 16:44:54 +0200

    Cleanup leftovers from previous deployments

--- test-server.sh
+++ test-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker-compose -f traefik/docker-compose.yaml --env-file ../.env up -d
-docker-compose -f portainer/docker-compose.yaml --env-file ../.env up -d
+docker-compose -f traefik/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+docker-compose -f portainer/docker-compose.yaml --env-file ../.env up -d --remove-orphans
