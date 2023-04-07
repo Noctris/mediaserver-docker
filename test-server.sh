8049d75bffb8699dc87c6d0140760959d0914494
Michel Van Hoof <m-vanhoof@lbctt.com>
2023-04-07 17:16:56 +0200

    Remove all the traefik stuff

--- test-server.sh
+++ test-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker-compose -f traefik/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+#docker-compose -f traefik/docker-compose.yaml --env-file ../.env up -d --remove-orphans
 docker-compose -f portainer-ee/docker-compose.yaml --env-file ../.env up -d --remove-orphans
