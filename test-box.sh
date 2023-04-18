088c03b6cb210fb2b18aced793e0dd515cec5e35
noctris <mvhoof@mail.com>
2023-04-18 09:50:28 +0200

    Update to switch dev box

--- test-box.sh
+++ test-box.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#docker-compose -f traefik/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+docker-compose -f traefik/docker-compose.yaml --env-file ../.env up -d --remove-orphans
 docker-compose -f monitoring/dozzle/docker-compose.yaml --env-file ../.env up -d --remove-orphans
 docker-compose -f portainer-ee/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+docker-compose -f infrastructure/emqx-cluster/docker-compose.yaml --env-file ../.env up -d --remove-orphans
