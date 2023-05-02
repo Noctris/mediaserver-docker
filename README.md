702d2f745dddfd0564f2f3bfff2ebed8bc92a312
noctris <mvhoof@gmail.com>
2023-05-02 12:49:24 +0200

    Updated to more of a stack setup

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # mediaserver-docker
 My Home Media Server in docker compose
+
+
+
+## TODO
+
+Check difference between Reslio Sync and Syncthing
+
+## TO Docker Compose:
+
+overseerr
+plex metadata manager
+lychee
+darktable (pics)
+resilo (sync with file stubbing)
+ntpd time server for the network
+
+Hassio to Seperate Server with addones?
+emqx for hassio and where to put it?
+
+## Home
+
+Netbootxyz
+
--- test-box.sh
+++ test-box.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-docker-compose -f traefik/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+docker-compose -p traefik -f traefik/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+docker-compose -f infrastructure/authentik/docker-compose.yaml --env-file ../.env up -d --remove-orphans -p authentik
 docker-compose -f monitoring/dozzle/docker-compose.yaml --env-file ../.env up -d --remove-orphans
-docker-compose -f portainer-ee/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+docker-compose -f management/portainer-ee/docker-compose.yaml --env-file ../.env up -d --remove-orphans
 docker-compose -f infrastructure/emqx-cluster/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+docker-compose -f smarthome/node-red/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+docker-compose -f smarthome/home-assistant/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+docker-compose -f infrastructure/samba/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+docker-compose -f infrastructure/adguard/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+docker-compose -f web/blog.vanhoof.me/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+docker-compose -f smarthome/home-panel/docker-compose.yaml --env-file ../.env up -d --remove-orphans
+docker-compose -p tabletpanel -f smarthome/home-panel/docker-compose.yaml --env-file ../.env up -d --remove-orphans
